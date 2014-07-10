b4bd7f900d14c2fca358cb32207a56850ac2bfae
Dmitriy Lois <epaman@rambler.ru>
2014-07-10 21:13:26 +0300

    commit unit1

---
+++ src/unit1/MyAdress.java
@@ -0,0 +1,12 @@
+package unit1;
+
+public class MyAdress {
+
+	public static void main(String[] args) {
+		System.out.println("Dmitriy Lois");
+		System.out.println("Polubotka 80");
+		System.out.println("Chernigov");
+		System.out.println("Ukraine");
+	}
+
+}
